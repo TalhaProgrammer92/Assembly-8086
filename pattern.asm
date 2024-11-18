138d06e3565067730e8cd758f13af80b53116236
Talha Ahmad <talha.code92@gmail.com>
2024-11-18 12:48:02 +0500

    Add files via upload
    This assembly code generates the following pattern: EMU8086.
    *
    **
    ***
    ****
    *****

---
+++ pattern.asm
@@ -0,0 +1,64 @@
+; Program to print the pattern:
+
+; *
+; **
+; ***
+; ****
+; *****
+
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+    count DW 1
+    char DB '*'
+    limit DW 5      ; You can change its value this is exactly the size of pattern
+    
+    ; Macro to print the character
+    PRINTC MACRO
+        MOV AH, 02H
+        MOV DL, char
+        INT 21H
+    PRINTC ENDM
+    
+    ; Macro to print a series of characters
+    PRINTS MACRO size
+        MOV CX, size
+        AGAIN:
+            PRINTC
+            LOOP AGAIN
+    PRINTS ENDM
+    
+    ; Macro for line break
+    ENDL MACRO
+        MOV DL, 0DH
+        MOV AH, 02H
+        INT 21H
+        MOV DL, 0AH
+        MOV AH, 02H
+        INT 21H
+    ENDL ENDM
+
+.CODE
+    MAIN PROC FAR
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        ; Print the pattern
+        PATTERN:
+            PRINTS count
+            ENDL
+            INC count
+            MOV AX, count
+            MOV BX, limit
+            CMP AX, BX
+            JLE PATTERN
+            JMP TERMINATE
+        
+        ; Terminate
+        TERMINATE:
+            MOV AH, 4CH
+            INT 21H
+    MAIN ENDP
+END MAIN
