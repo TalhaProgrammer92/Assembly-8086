55f009f122d38aedf70614867b67dc0e3a93c61b
Talha Ahmad <talha.code92@gmail.com>
2024-11-18 09:10:21 +0500

    Add files via upload
    Dive into the fascinating world of assembly programming with a fun challenge: write a program that determines whether a given character is a vowel or a consonant! Unleash your coding skills and put your logic to the test as you navigate through this engaging task. Get ready to see how your program can make sense of letters and classify them with precision!

---
+++ vowel_consonant.asm
@@ -0,0 +1,91 @@
+; Programt to check whether an input character is vowel or consonant
+
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+    msg DB 'Enter a character>> $'
+    vowel DB 'It is vowel$'
+    consonant DB 'It is consonant$'
+    char DB ?
+    
+    ; Macro to show the message
+    SHOW MACRO message
+        MOV AH, 09H
+        LEA DX, message
+        INT 21H
+    SHOW ENDM
+    
+    ; Macro to input the character
+    GET MACRO
+        MOV AH, 01H
+        INT 21H
+        MOV char, AL
+    GET ENDM
+    
+    ; Macro for line break
+    ENDL MACRO
+        MOV DL, 0DH
+        MOV AH, 02H
+        INT 21H
+        MOV DL, 0AH
+        MOV AH, 02H
+        INT 21H
+    ENDL ENDM
+    
+    ; Macro to terminate the program
+    TERMINATE MACRO
+        MOV AH, 04H
+        INT 21H
+    TERMINATE ENDM
+    
+    ; Macro to check whether it is vowel or consonant
+    RESULT MACRO
+        ENDL
+        
+        ; Compare
+        CMP char, 'a'   ; Compare char with 'a'
+        JE VWL          ; As: if char = 'a' then jump [label]
+        CMP char, 'A'
+        JE VWL
+        CMP char, 'e'
+        JE VWL
+        CMP char, 'E'
+        JE VWL
+        CMP char, 'i'
+        JE VWL
+        CMP char, 'I'
+        JE VWL
+        CMP char, 'o'
+        JE VWL
+        CMP char, 'O'
+        JE VWL
+        CMP char, 'U'
+        JE VWL
+        CMP char, 'u'
+        JE VWL          ; True-case
+        JMP CNT         ; False-case, As: jump [label]
+    RESULT ENDM
+
+.CODE
+    MAIN PROC FAR
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        ; Operation
+        SHOW msg
+        GET
+        RESULT
+        
+        ; Block
+        VWL:
+            SHOW vowel
+            TERMINATE
+        CNT:
+            SHOW consonant
+        
+        ; Termination
+        TERMINATE
+    MAIN ENDP
+END MAIN
