7b811ce4aa3492bea8a6fab9964002d20901ecc6
Talha Ahmad <talha.code92@gmail.com>
2024-12-11 15:36:03 +0500

    Add files via upload
    Recently, I embarked on a project to create a simple yet classic game: Tic Tac Toe, using C++. After gaining a solid understanding of Assembly language, I found it intriguing to challenge myself by developing the same game in Assembly. This led me to explore the nuances of low-level programming while recreating a familiar game. Below, you will find the source code for my Assembly version of Tic Tac Toe. I invite you to play and enjoy the experience!

---
+++ tic_tac_game.asm
@@ -0,0 +1,376 @@
+; Tic Tac Game - Created By: Talha Ahmad (c)
+
+; By Default: Player 1 -> O | Player 2 -> X
+
+.MODEL SMALL
+
+.STACK 100H
+
+.DATA
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Game Variables
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; Board
+    board DB '123456789$'
+    location DB ?
+    
+    ; In-game
+    turn DB 0
+    game_over_flag DB 0
+    draw_flag DB 0
+    symbol DB ?
+    winner DB ?
+    
+    ; Prompts
+    p1_prompt DB 'Turn of Player 1: $'
+    p2_prompt DB 'Turn of Player 2: $'
+    
+    ; Messages on game over
+    p1_won_msg DB 'Player 1 (O) won the game!$'
+    p2_won_msg DB 'Player 2 (X) won the game!$'
+    draw_msg DB 'Game is Draw!$'
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ; Definition of Macro
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; Display a string
+    PRINTS MACRO string
+        MOV AH, 09H
+        LEA DX, string
+        INT 21H
+    PRINTS ENDM
+    
+    ; Display a character
+    PRINTC MACRO char
+        MOV DL, char
+        MOV AH, 02H
+        INT 21H
+    PRINTC ENDM
+    
+    ; Line Break
+    ENDL MACRO
+        PRINTC 0DH
+        PRINTC 0AH
+    ENDL ENDM
+    
+    ; Display game board
+    DISPLAY MACRO
+        MOV SI, 0
+        DISP_LOOP:
+            PRINTC ' '
+            PRINTC board[SI]
+            PRINTC ' '
+            INC SI
+            
+            ; Line Break - Control
+            CMP SI, 3
+            JE BREAK_DL
+            CMP SI, 6
+            JE BREAK_DL
+            JMP CONT_DL
+            
+            BREAK_DL:
+                ENDL
+            
+            CONT_DL:
+                CMP board[SI], '$'
+                JNE DISP_LOOP
+        ENDL
+    DISPLAY ENDM
+    
+    ; Announce Winner / Draw
+    ANNOUNCE MACRO
+        
+        ENDL
+        
+        ; Display the board
+        MOV SI, 0
+        BOARD_LOOP:
+            PRINTC ' '
+            PRINTC board[SI]
+            PRINTC ' '
+            INC SI
+            
+            ; Line Break - Control
+            CMP SI, 3
+            JE BREAK_BL
+            CMP SI, 6
+            JE BREAK_BL
+            JMP CONT_BL
+            
+            BREAK_BL:
+                ENDL
+            
+            CONT_BL:
+                CMP board[SI], '$'
+                JNE BOARD_LOOP
+        ENDL
+        
+        ; Winner
+        CMP draw_flag, 1
+        JE DRAW_CASE
+        CMP winner, 'O'
+        JE P1_WON
+        JMP P2_WON
+        
+        DRAW_CASE:
+            PRINTS draw_msg
+            JMP EXIT
+        
+        P1_WON:
+            PRINTS p1_won_msg
+            JMP EXIT
+        P2_WON:
+            PRINTS p2_won_msg
+            JMP EXIT
+    ANNOUNCE ENDM
+
+.CODE
+    MAIN PROC FAR
+        MOV AX, @DATA
+        MOV DS, AX
+        
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        ; Main loop of the game
+        ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+        GAME_LOOP:
+            
+            ; Display Game Board
+            ENDL
+            DISPLAY
+
+            ; Display player's turn prompt
+            CMP turn, 0
+            JE P1_DT
+            JMP P2_DT
+        
+            P1_DT:
+                PRINTS p1_prompt
+                MOV symbol, 'O'
+                INC turn               
+                JMP INPUT
+        
+            P2_DT:
+                PRINTS p2_prompt
+                MOV symbol, 'X'
+                DEC turn
+            
+            ; Get input from the player
+            INPUT:
+                MOV AH, 01H
+                INT 21H
+                MOV location, AL
+                ENDL
+                
+            ; Place symbol
+            PLACE:
+                CMP location, '1'
+                JE P_1
+                CMP location, '2'
+                JE P_2
+                CMP location, '3'
+                JE P_3
+                CMP location, '4'
+                JE P_4
+                CMP location, '5'
+                JE P_5
+                CMP location, '6'
+                JE P_6
+                CMP location, '7'
+                JE P_7
+                CMP location, '8'
+                JE P_8
+                CMP location, '9'
+                JE P_9
+                JMP CHECK
+                
+                P_1:
+                    CMP board[0], '1'
+                    JE P1_OK
+                    JMP CHECK
+                    
+                    P1_OK:
+                        MOV AL, symbol
+                        MOV board[0], AL
+                        JMP CHECK
+                
+                P_2:
+                    CMP board[1], '2'
+                    JE P2_OK
+                    JMP CHECK
+                    
+                    P2_OK:
+                        MOV AL, symbol
+                        MOV board[1], AL
+                        JMP CHECK
+                
+                P_3:
+                    CMP board[2], '3'
+                    JE P3_OK
+                    JMP CHECK
+                    
+                    P3_OK:
+                        MOV AL, symbol
+                        MOV board[2], AL
+                        JMP CHECK
+                
+                P_4:
+                    CMP board[3], '4'
+                    JE P4_OK
+                    JMP CHECK
+                    
+                    P4_OK:
+                        MOV AL, symbol
+                        MOV board[3], AL
+                        JMP CHECK
+                
+                P_5:
+                    CMP board[4], '5'
+                    JE P5_OK
+                    JMP CHECK
+                    
+                    P5_OK:
+                        MOV AL, symbol
+                        MOV board[4], AL
+                        JMP CHECK
+                
+                P_6:
+                    CMP board[5], '6'
+                    JE P6_OK
+                    JMP CHECK
+                    
+                    P6_OK:
+                        MOV AL, symbol
+                        MOV board[5], AL
+                        JMP CHECK
+                        
+                P_7:
+                    CMP board[6], '7'
+                    JE P7_OK
+                    JMP CHECK
+                    
+                    P7_OK:
+                        MOV AL, symbol
+                        MOV board[6], AL
+                        JMP CHECK
+                        
+                P_8:
+                    CMP board[7], '8'
+                    JE P8_OK
+                    JMP CHECK
+                    
+                    P8_OK:
+                        MOV AL, symbol
+                        MOV board[7], AL
+                        JMP CHECK
+                
+                P_9:
+                    CMP board[8], '9'
+                    JE P9_OK
+                    JMP CHECK
+                    
+                    P9_OK:
+                        MOV AL, symbol
+                        MOV board[8], AL
+                        JMP CHECK
+            
+            ; Check winner
+            CHECK:
+                
+                ; Row Check
+                MOV SI, 0
+                ROW_LOOP:
+                    MOV AL, board[SI]
+                    CMP board[SI + 1], AL
+                    JE RNEXT
+                    JMP RCONT
+                    
+                    RNEXT:
+                        CMP board[SI + 2], AL
+                        MOV winner, AL
+                        JE GAME_IS_OVER
+                    
+                    RCONT:
+                        ADD SI, 3
+                        CMP SI, 6
+                        JLE ROW_LOOP
+
+                ; Column Check
+                MOV SI, 0
+                COLUMN_LOOP:
+                    MOV AL, board[SI]
+                    CMP board[SI + 3], AL
+                    JE CNEXT
+                    JMP CCONT
+                    
+                    CNEXT:
+                        CMP board[SI + 6], AL
+                        MOV winner, AL
+                        JE GAME_IS_OVER
+                    
+                    CCONT:
+                        INC SI
+                        CMP SI, 3
+                        JLE COLUMN_LOOP
+
+                ; Diagonol Check
+                MOV SI, 4
+                MOV AL, board[SI]
+                LR_CHK:
+                    CMP board[0], AL
+                    JE LRNEXT
+                    JMP RL_CHK
+                    
+                    LRNEXT:
+                        CMP board[8], AL
+                        MOV winner, AL
+                        JE GAME_IS_OVER
+                RL_CHK:
+                    CMP board[2], AL
+                    JE RLNEXT
+                    JMP DRAW_CHK
+                    
+                    RLNEXT:
+                        CMP board[6], AL
+                        MOV winner, AL
+                        JE GAME_IS_OVER
+                        JMP DRAW_CHK
+                    
+                ; Draw Check
+                DRAW_CHK:
+                    MOV SI, 0
+                    DCHK_LOOP:
+                        CMP board[SI], 'O'
+                        JNE DCHK_X
+                        JMP DCHK_CONT
+                        DCHK_X:
+                            CMP board[SI], 'X'
+                            JE DCHK_CONT
+                            JMP GL_CONT
+                        DCHK_CONT:
+                            INC SI
+                            CMP SI, 9
+                            JL DCHK_LOOP
+                    MOV draw_flag, 1
+                    JMP GAME_IS_OVER
+            
+            ; Game Over - Flag -> True
+            GAME_IS_OVER:
+                MOV game_over_flag, 1
+            
+            ; Check if game is over
+            GL_CONT:
+                CMP game_over_flag, 1
+                JNE GAME_LOOP
+        
+        ; Announce Winner / Draw
+        ANNOUNCE
+        
+        ; Terminate the program
+        EXIT:
+            MOV AH, 4CH
+            INT 21H
+    MAIN ENDP
+END MAIN
