45644c5d1ee53b02cd70394cb446b8b0b0ac4c0b
Talha Ahmad <talha.code92@gmail.com>
2025-05-12 07:58:46 +0500

    Update Tic Tac Toe - Advance.asm

--- Tic Tac Toe - Advance.asm
+++ Tic Tac Toe - Advance.asm
@@ -22,6 +22,7 @@
     msg_draw DB 'Game is Draw$'     ; When display draw messsage
     
     alert_invalid_cell DB 'Cell number must be between 1 and 9!$'                                   ; When player enter invalid cell number for game board
+    alert_taken_cell DB 'Cell is not empty!$'                                                       ; When the selescted cell is not empty
     alert_invalid_symbol DB 'You must select upper case alphabet between A and Z as symbol!$'       ; When player enter invalid symbol like 1, [
     alert_existed_symbol DB 'The symbol you selected has already been taken by your opponent!$'     ; When player enter symbol which is already taken
     
@@ -36,10 +37,11 @@
     valid DB ?      ; Check validity
     
     ; In-game (Variables)
-    winner DB ?     ; Winner
+    winner DB ?     ; Winner | n - None | d - Draw | [any symbol A-Z] - Player who is winner
     input DB ?      ; Player/User Input
     
-    player_symbol DB 2 DUP('$') ; Player - Symbol
+    player_symbol DB ?, ?       ; Player - Symbol
+    player_score DB 0, 0        ; Player - Scores (Matches Won)
     
     ;;;;;;;;;;
     ; Macros ;
@@ -92,6 +94,13 @@
         XOR turn, 1
     UPDATE_TURN ENDM
     
+    ; Reset flags
+    RESET_FLAGS MACRO
+        MOV game_over, 0
+        MOV winner, 'n'
+        UPDATE_TURN
+    RESET_FLAGS ENDM
+    
     ; Clear Screen
     CLRSCR MACRO
         MOV AH, 00H
@@ -111,9 +120,7 @@
         MOV AX, @DATA
         MOV DS, AX               
         
-        ;CALL START_GAME
-        CALL RESET_BOARD
-        CALL DISPLAY_BOARD
+        CALL START_GAME
         
         ; Program Termination
         MOV AH, 4CH
@@ -181,6 +188,7 @@
                 LINE_BREAK
                 PRINTS alert_invalid_symbol
                 LINE_BREAK
+                LINE_BREAK
                 
                 JMP SS_PR1
         
@@ -214,6 +222,7 @@
                 LINE_BREAK
                 PRINTS alert_invalid_symbol
                 LINE_BREAK
+                LINE_BREAK
                 
                 JMP SS_PR2
         
@@ -243,16 +252,20 @@
     
     ; Reset Board - Contains bug
     RESET_BOARD PROC
-        MOV SI, 0
-        MOV AL, '0'
+        MOV SI, 0 
+        MOV AL, 49  ; '1'
         RB_LOOP:
             MOV board[SI], AL
             
             INC AL
             
             INC SI
+            
             CMP SI, 9
             JL RB_LOOP
+        
+        ; Reset all game flags
+        RESET_FLAGS
         
         RET
     RESET_BOARD ENDP
@@ -268,6 +281,7 @@
                 PRINTC board[SI]
                 PRINTC ' '
                 
+                INC SI
                 CMP BL, 2
                 JB DBI_CS
                 JMP DBI_CONTROL
@@ -297,6 +311,112 @@
         RET
     DISPLAY_BOARD ENDP
     
+    ; Display turn of a player
+    DISPLAY_TURN PROC
+        LINE_BREAK
+        
+        PRINTS msg_turn
+        
+        MOV BX, 0
+        MOV BL, turn
+        
+        PRINTC player_symbol[BX]
+        
+        LINE_BREAK
+        LINE_BREAK
+        
+        RET     
+    DISPLAY_TURN ENDP
+    
+    ; Check if the selected cell is empty or not - result will be stored in 'valid'
+    IS_CELL_EMPTY PROC
+        ; Preset valid to 0
+        MOV valid, 0
+        
+        ; Get 'input' - Check input value
+        MOV AL, input
+        ADD AL, '0'
+        
+        ; To check value at index of board w.r.t input
+        MOV BX, 0
+        MOV BL, input
+        DEC BL
+        
+        ; Check if both are not same
+        CMP AL, board[BX]
+        JE ICE_EMPTY    ; Yes
+        JMP ICE_EXIT    ; No
+        
+        ; If input != board[input - 1]
+        ICE_EMPTY:
+            MOV valid, 1
+        
+        ICE_EXIT:
+            RET      
+    IS_CELL_EMPTY ENDP
+    
+    ; Take input - value will be store in 'input' memory location
+    TAKE_INPUT PROC
+        
+        TI_LOOP:
+            ; Prompt the player
+            PRINTS prompt_input
+            INPUTN input
+            LINE_BREAK
+            
+            ; Check if input > 0
+            CMP input, 0
+            JA TIL_CONFIRM  ; Yes
+            JMP TIL_ERROR   ; No
+            
+            ; Check if input > 9
+            TIL_CONFIRM:
+                CMP input, 9
+                JA TIL_ERROR    ; Yes
+                JMP TI_ICE      ; No
+            
+            ; If 'input' does not lie between 1 and 9
+            TIL_ERROR:
+                PRINTS alert_invalid_cell
+                LINE_BREAK
+                LINE_BREAK
+                JMP TI_LOOP
+            
+            ; Check if the selected cell is empty or not
+            TI_ICE:
+                CALL IS_CELL_EMPTY
+                CMP valid, 1
+                JE TI_EXIT  ; Yes
+                
+                PRINTS alert_taken_cell ; No
+                LINE_BREAK
+                LINE_BREAK
+                JMP TI_LOOP
+        
+        TI_EXIT:
+            RET
+    TAKE_INPUT ENDP
+    
+    ; Place Symbol in the selected cell
+    PLACE_SYMBOL PROC
+        ; Get 'turn'
+        MOV BX, 0
+        MOV BL, turn
+        
+        ; Get symbol
+        MOV AL, player_symbol[BX]
+        
+        ; Get 'input'
+        MOV BX, 0
+        MOV BL, input
+        DEC BL
+        
+        ; Place symbol
+        MOV board[BX], AL
+        
+        RET
+    PLACE_SYMBOL ENDP
+    
     ; Multiplayer Game
     MULTIPLAYER_GAME PROC
         ; Select Symbol
@@ -305,7 +425,32 @@
         
         ; Reset Board
         CALL RESET_BOARD
-        CALL DISPLAY_BOARD
+        
+        ; Game Loop
+        MG_LOOP:
+            ; Clear Screen
+            CLRSCR
+            
+            ; Display Board
+            CALL DISPLAY_BOARD
+            
+            ; Display turn
+            CALL DISPLAY_TURN
+            
+            ; Take input
+            CALL TAKE_INPUT
+            
+            ; Place symbol
+            CALL PLACE_SYMBOL
+            
+            ; Check Winner
+            
+            ; Update turn
+            UPDATE_TURN
+            
+            ; Loop Control
+            CMP game_over, 0
+            JE MG_LOOP
         
         RET
     MULTIPLAYER_GAME ENDP
@@ -315,6 +460,9 @@
         ; Select Symbol
         CALL SYMBOL_SELECTION
         CLRSCR
+        
+        ; Reset Board
+        CALL RESET_BOARD
         
         RET
     COMPUTER_GAME ENDP
