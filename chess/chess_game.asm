3fab8d703050fe8ecdb34356137b28b15bbb2f92
Talha Ahmad <talha.ahmad.tech@gmail.com>
2026-05-15 09:42:42 +0500

    Procedure to clear the Board and place Pieces.
    I added two procedures to clear the board and place all pieces to their initial positions on the board.

--- chess/chess_game.asm
+++ chess/chess_game.asm
@@ -62,6 +62,20 @@
     selected_piece DB ?
     board_index DB ?
     
+    white_pawn DB 'P'
+    white_knight DB 'N'
+    white_bishop DB 'B'
+    white_rook DB 'R'
+    white_queen DB 'Q'
+    white_king DB 'K'
+    
+    black_pawn DB 'p'
+    black_knight DB 'n'
+    black_bishop DB 'b'
+    black_rook DB 'r'
+    black_queen DB 'q'
+    black_king DB 'k'
+    
     ; [0] - Row | [1] - Column
     current_position DB ?, ?
     target_position DB ?, ?
@@ -121,12 +135,102 @@
         MOV AX, @DATA
         MOV DS, AX
         
-        ; Display a welcome message
-        PRINTS welcome_msg
+        ; Testing...
+        CALL CLEAR_BOARD
+        CALL PLACE_PIECES
         
         ; Terminating the program
         MOV AH, 4ch
         INT 21h
     MAIN ENDP
+    
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    ;                           BOARD PROCEDURES                               ;
+    ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+    
+    ; <----- Clear the game board -----> ;
+    CLEAR_BOARD PROC
+        MOV SI, 0
+        CB_LOOP:
+            MOV board[SI], '-'
+            INC SI
+            CMP SI, 64
+            JB CB_LOOP
+        
+        RET
+    CLEAR_BOARD ENDP
+    
+    ; <----- Place all pieces on the board -----> ;
+    PLACE_PIECES PROC
+        ;;;;;;;;;; WHITE PIECES ;;;;;;;;;;
+        
+        ; Pawns
+        MOV SI, 8
+        MOV CX, 8
+        MOV AL, white_pawn
+        PPWP_LOOP:
+            MOV board[SI], AL
+            INC SI
+            LOOP PPWP_LOOP 
+        
+        ; Knights
+        MOV AL, white_knight
+        MOV board[1], AL
+        MOV board[6], AL
+        
+        ; Bishops
+        MOV AL, white_bishop
+        MOV board[2], AL
+        MOV board[5], AL
+        
+        ; Rooks
+        MOV AL, white_rook
+        MOV board[0], AL
+        MOV board[7], AL
+        
+        ; Queen
+        MOV AL, white_queen
+        MOV board[3], AL
+        
+        ; King
+        MOV AL, white_king
+        MOV board[4], AL
+        
+        ;;;;;;;;;; BLACK PIECES ;;;;;;;;;;
+        
+        ; Pawns
+        MOV SI, 48
+        MOV CX, 8
+        MOV AL, black_pawn
+        PPBP_LOOP:
+            MOV board[SI], AL
+            INC SI
+            LOOP PPBP_LOOP 
+        
+        ; Knights
+        MOV AL, black_knight
+        MOV board[57], AL
+        MOV board[62], AL
+        
+        ; Bishops
+        MOV AL, black_bishop
+        MOV board[58], AL
+        MOV board[61], AL
+        
+        ; Rooks
+        MOV AL, black_rook
+        MOV board[56], AL
+        MOV board[63], AL
+        
+        ; Queen
+        MOV AL, black_queen
+        MOV board[59], AL
+        
+        ; King
+        MOV AL, white_king
+        MOV board[60], AL
+        
+        RET
+    PLACE_PIECES ENDP
 
 END MAIN
